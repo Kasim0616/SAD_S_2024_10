f46f0a5c25223474493c4f9694061d573e9adf3f
kasim0616 <nihal1662001@gmail.com>
2025-05-29 16:07:11 +0200

    Dip 01 completed

--- SOLID/DIP_01.java
+++ SOLID/DIP_01.java
@@ -2,36 +2,42 @@ package SOLID;
 
 public class DIP_01 {
 
-    /*
-     * TASK:
-     * The Switch is violating the DIP (Dependency Inversion Principle).
-     * Please fix this!
-     */
-    
-    public static class LightBulb {
+    // Step 1: Define an abstraction
+    public interface SwitchableDevice {
+
+        void turnOn();
+
+        void turnOff();
+    }
+
+    // Step 2: Implement the interface
+    public static class LightBulb implements SwitchableDevice {
+
         public void turnOn() {
             System.out.println("Light is ON!");
         }
+
         public void turnOff() {
             System.out.println("Light is OFF!");
         }
     }
 
+    // Step 3: Switch depends on the abstraction
     public static class Switch {
-        private LightBulb lightBulb;
 
-        // this is "Dependency Injection" (composition style)
-        public Switch(LightBulb lightBulb) {
-            this.lightBulb = lightBulb;
+        private SwitchableDevice device;
+
+        public Switch(SwitchableDevice device) {
+            this.device = device;
         }
 
         public void operate() {
-            lightBulb.turnOn();
+            device.turnOn();
         }
     }
 
     public static void main(String[] args) {
-        LightBulb lightBulb = new LightBulb();
+        SwitchableDevice lightBulb = new LightBulb();
         Switch lightSwitch = new Switch(lightBulb);
         lightSwitch.operate();
     }
