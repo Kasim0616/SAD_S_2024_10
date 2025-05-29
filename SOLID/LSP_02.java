216c67c1840e7910673fd29e28c3a8288f0a4ddb
kasim0616 <nihal1662001@gmail.com>
2025-05-29 16:27:23 +0200

    LSP 2 completed

--- SOLID/LSP_02.java
+++ SOLID/LSP_02.java
@@ -1,40 +1,49 @@
 package SOLID;
 
 public class LSP_02 {
-    
-    /*
-     * TASK:
-     * The polymorphic function move expect the same behaviour
-     * for all birds, but this isn't the case.
-     * Fix this to obey the LSP (Liskov Substitution Principle)!
-     */
 
-    public static class Bird {
-        public void fly() {
-            System.out.println("The bird is flying...");
-        }
+    public static abstract class Bird {
+
+        public abstract void makeSound();
     }
 
-    public static class Sparrow extends Bird {
+    public static abstract class FlyingBird extends Bird {
+
+        public abstract void fly();
+    }
+
+    public static class Sparrow extends FlyingBird {
+
         @Override
         public void fly() {
             System.out.println("The sparrow is flying...");
         }
-    }
 
-    public static class Penguin extends Bird {
         @Override
-        public void fly() {
-            throw new UnsupportedOperationException("Penguins can't fly :(");
+        public void makeSound() {
+            System.out.println("Chirp chirp!");
         }
     }
 
-    public static void move(Bird bird) {
+    public static class Penguin extends Bird {
+
+        @Override
+        public void makeSound() {
+            System.out.println("Honk honk!");
+        }
+
+        public void swim() {
+            System.out.println("The penguin is swimming...");
+        }
+    }
+
+    public static void move(FlyingBird bird) {
         bird.fly();
     }
 
     public static void main(String[] args) {
-        move(new Sparrow());
-        move(new Penguin());
-    }
-}
+        Sparrow sparrow = new Sparrow();
+        move(sparrow);
+
+        Penguin penguin = new Penguin();
+        penguin.swim();
